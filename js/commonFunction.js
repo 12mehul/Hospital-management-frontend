05f68b9d39b9d4f4641558bbd18959b5eff02121
12mehul <mehul091298@gmail.com>
2025-02-25 03:06:45 +0530

    api url change

--- js/commonFunction.js
+++ js/commonFunction.js
@@ -1,7 +1,7 @@
 import { logout } from "./storeToken.js";
 
 export const onlineApiUrl =
-  "https://hospital-management-backend-theta.vercel.app/api";
+  "https://hospital-management-backend-tawny.vercel.app/api";
 export const offlineApiUrl = "http://localhost:5000/api";
 
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
